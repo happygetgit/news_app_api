f6533122c068cd1858224305f2935e8f4958469b
happygetgit <sk39happy@gmail.com>
2026-03-18 23:28:27 +0530

    added sampleOutput json , also a layout for the newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
-    return <div>This is a news Component</div>;
+    return (
+      <div className="container my-3">
+        <h2>NewsMonkey - TopHeadlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>This is a new Item component</div>;
+    let { title, description } = this.props;
+    return (
+      <div className="card" style={{ width: "18rem" }}>
+        <img
+          src="https://ichef.bbci.co.uk/news/1024/branded_news/3f4d/live/d8eacbe0-21cb-11f1-a79a-77e93010d956.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/newsdetail" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
   }
 }
 
